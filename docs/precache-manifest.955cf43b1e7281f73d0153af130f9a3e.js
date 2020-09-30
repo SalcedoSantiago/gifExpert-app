6807417d9b9b4aa751c733ba257f784d8baa5383
Santiago Dalmiro Salcedo <santiisalcedo129@gmail.com>
2020-09-30 13:55:03 -0300

    index.html actualizado

---
+++ docs/precache-manifest.955cf43b1e7281f73d0153af130f9a3e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "139df2966199e4e8f8616f54bc9f49e5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3eb08e3cef68a7cd482d",
+    "url": "/static/js/2.1750bb7d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.1750bb7d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c603b6f62602521a1cc9",
+    "url": "/static/js/main.3f2c22bc.chunk.js"
+  },
+  {
+    "revision": "3f84125b0ad7a06806bd",
+    "url": "/static/js/runtime-main.148bb96f.js"
+  }
+]);
